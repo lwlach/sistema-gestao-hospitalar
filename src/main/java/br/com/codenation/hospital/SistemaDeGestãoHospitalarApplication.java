4201c9122c5775cc1406926e936c8e850fa33094
aacgn <4augusto@gmail.com>
2019-03-16 11:54:25 -0300

    Add project core

---
+++ src/main/java/br/com/codenation/hospital/SistemaDeGestãoHospitalarApplication.java
@@ -0,0 +1,13 @@
+package br.com.codenation.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestãoHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestãoHospitalarApplication.class, args);
+	}
+
+}
